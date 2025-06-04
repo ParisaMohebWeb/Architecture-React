b54480c99f0e5c9bfca3bdc0a0cb244525b7c923
Mrs Moheb <parisa.mohebweb.gmail.com>
2025-06-04 17:52:25 +0330

    enter gallery part

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 import "./App.css";
 
 import homeImage from "./assets/img/home-image1.webp";
+import image1 from "./assets/img/1.webp";
+import image11 from "./assets/img/11.webp";
+import image222 from "./assets/img/222.jpg";
+import image22 from "./assets/img/22.webp";
+import image2222 from "./assets/img/2222.jpg";
+import image33 from "./assets/img/33.webp";
+import image4 from "./assets/img/4.webp";
+import image44 from "./assets/img/44.webp";
+import image5 from "./assets/img/5.webp";
+import image55 from "./assets/img/55.webp";
+import image6 from "./assets/img/6.webp";
+import image66 from "./assets/img/66.webp";
+
+
 
 function App() {
   return (
@@ -136,18 +150,103 @@ function App() {
         <p>PROJECT MANAGEMENT</p>
       </section>
 
- <section className="part3">
+      <section className="part3">
+        <div>
+          <h3> RECENT PROJECTS </h3>
+        </div>
+        <div></div>
+        <div>
+          <p>
+            {" "}
+            Avada & Avada is an award winning company with a reputation for
+            consistent innovation at the highest level of design.
+          </p>
+        </div>
+      </section>
 
+      <section className="gallury">
+        <div>
+          <div>
+            <figure>
+              <a href="#">
+                <img src={image1} alt="" />
+                <img src={image11} alt="" />
+              </a>
+            </figure>
             <div>
-                <h3> RECENT PROJECTS </h3>
+              <a href="#">Sunrise Avenue</a>
+              <p>Commercial, Industrial</p>
             </div>
-            <div></div>
-            <div>
-                <p> Avada & Avada is an award winning company with a reputation for consistent innovation at the highest
-                    level of design.</p>
-            </div>
-        </section>
+          </div>
 
+          <div>
+            <figure>
+              <a href="#">
+                <img src={image222} alt="" />
+                <img src={image22} alt="" />
+              </a>
+            </figure>
+            <div>
+              <a href="#">Singapore Skyrise</a>
+              <p>Commercial, Industrial</p>
+            </div>
+          </div>
+
+          <div>
+            <figure>
+              <a href="#">
+                <img src={image2222} alt="" />
+                <img src={image33} alt="" />
+              </a>
+            </figure>
+            <div>
+              <a href="#">Sunrise Avenue</a>
+              <p>Commercial, Industrial</p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <figure>
+              <a href="#">
+                <img src={image4} alt="" />
+                <img src={image44} alt="" />
+              </a>
+            </figure>
+            <div>
+              <a href="#">Danish Modernity</a>
+              <p>Commercial, Industrial</p>
+            </div>
+          </div>
+
+          <div>
+            <figure>
+              <a href="#">
+                <img src={image5} alt="" />
+                <img src={image55} alt="" />
+              </a>
+            </figure>
+            <div>
+              <a href="#">West Shinjuku</a>
+              <p>Commercial, Industrial</p>
+            </div>
+          </div>
+
+          <div>
+            <figure>
+              <a href="#">
+                <img src={image6} alt="" />
+                <img src={image66} alt="" />
+              </a>
+            </figure>
+            <div>
+              <a href="#">Manchester Airport</a>
+              <p>Commercial, Industrial</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
